8d929e91b99e7d7d90648a58ce59cb6167e5152f
Viacheslav Kudinov <viacheslav.kudinov@gmail.com>
2023-01-17 13:35:46 +0100

    Application has been changed

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,29 +1,13 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-        System.out.println("A NullPointerException is a drag...");
-        Object myObject = null;
-        /**
-         * Checker prevents this from compiling...
-         */
-//        System.out.println("myObject: " + myObject.toString());
-        /**
-         * ... which is simply fantastic. It shows:
-         *
-         * error: [dereference.of.nullable] dereference of possibly-null
-         * reference myObject
-         *
-         * http://checkerframework.org
-         */
-        System.out.println("... but thankfully, Checker has our back: http://checkerframework.org");
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
+
 }
